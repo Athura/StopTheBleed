be32a8b254aba2bbcef4ba3f4ff6ced69026a9f4
Athura <joshualjohnson33@gmail.com>
2019-05-10 08:07:10 -0400

    Finish delete class

--- backend/routes/class.js
+++ backend/routes/class.js
@@ -1,80 +1,142 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const auth = require("../middleware/auth");
-const request = require("request");
-const config = require("config");
-const { check, validationResult } = require("express-validator/check");
+const auth = require('../middleware/auth');
+const request = require('request');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
 
-const Class = require("../models/Class");
-const User = require("../models/User");
+const Class = require('../models/Class');
+const User = require('../models/User');
 
 // @route     GET api/class
 // @desc      Test route for class
 // @access    public
-router.get("/", (req, res) => res.send("Class route working as expected"));
+router.get('/', (req, res) => res.send('Class route working as expected'));
 
 // @route     POST api/class
 // @desc      Create or update a new class
 // @access    private
-router.post("/", auth, [
-    check("description", "Please provide a description of what will be taught.").not().isEmpty(),
-    check("name", "Please provide a name for your class").not().isEmpty(),
-    check("sizeLimit", "Please provide a number for how many students can enroll in your class.").not().isEmpty(),
-    check("location", "Please provide where your class will be taking place.").not().isEmpty()
-], async (req, res) => {
+router.post(
+  '/',
+  auth,
+  [
+    check('description', 'Please provide a description of what will be taught.')
+      .not()
+      .isEmpty(),
+    check('name', 'Please provide a name for your class')
+      .not()
+      .isEmpty(),
+    check(
+      'sizeLimit',
+      'Please provide a number for how many students can enroll in your class.'
+    )
+      .not()
+      .isEmpty(),
+    check('location', 'Please provide where your class will be taking place.')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
-    const user = await User.findById(req.user.id).select("-password");
+    const user = await User.findById(req.user.id).select('-password');
     if (!errors.isEmpty()) {
-        return res.status(400).json({
-            errors: errors.array()
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+
+    if (user.userType !== 'teacher') {
+      res
+        .status(500)
+        .send({
+          msg:
+            'You must be a teacher to create a class. Please edit your account user type.'
         });
     }
 
-    if(user.userType !== "teacher") {
-        res.status(500).send({ msg: "You must be a teacher to create a class. Please edit your account user type." });
-    }
+    const { description, name, sizeLimit, status, location, rating } = req.body;
 
-        const { description, name, sizeLimit, status, location, rating } = req.body;
+    const classFields = {};
 
-        const classFields = {};
-
-        if (description) classFields.description = description;
-        if (name) classFields.name = name;
-        if (sizeLimit) classFields.sizeLimit = sizeLimit;
-        if (status) classFields.status = status;
-        if (location) classFields.location = location;
-        if (rating) classFields.rating = rating;
+    if (description) classFields.description = description;
+    if (name) classFields.name = name;
+    if (sizeLimit) classFields.sizeLimit = sizeLimit;
+    if (status) classFields.status = status;
+    if (location) classFields.location = location;
+    if (rating) classFields.rating = rating;
 
     try {
-        let newClass = await Class.findOne({
-            user: req.user.id
+      let newClass = await Class.findOne({
+        user: req.user.id
+      });
+
+      newClass = new Class({
+        description,
+        name,
+        sizeLimit,
+        status,
+        location,
+        rating,
+        user: req.user.id
+      });
+
+      await newClass.save();
+      res.json(newClass);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+router.put('/:class_id', auth, async (req, res) => {
+  try {
+    const updateClass = await Class.findOne({
+      class: req.params.class_id
+    });
+
+    if (!updateClass) {
+      res.status(404).send({ msg: 'Class could not be found' });
+    }
+
+    const { description, name, sizeLimit, status, location, rating } = req.body;
+
+    const newClassFields = {};
+
+    if (description) newClassFields.description = description;
+    if (name) newClassFields.name = name;
+    if (sizeLimit) newClassFields.sizeLimit = sizeLimit;
+    if (status) newClassFields.status = status;
+    if (location) newClassFields.location = location;
+    if (rating) newClassFields.rating = rating;
+
+    await updateClass.save(newClassFields);
+    res.json(updateClass);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route     DELETE api/class/class_id
+// @desc      Delete Class by ID
+// @access    private
+router.delete('/:class_id', auth, async (req, res) => {
+    try {
+        await Class.findOneAndRemove({
+            _id: req.params.class_id
         });
 
-        newClass = new Class({
-            description,
-            name,
-            sizeLimit,
-            status,
-            location,
-            rating,
-            user: req.user.id
-        });
+        // const removeIndex = deleteClass._id.map(item => item.id).indexOf(req.params.class_id);
 
-        await newClass.save();
-        res.json(newClass);
+        // deleteClass.class.splice(removeIndex, 1);
+
+        res.json({
+            msg: 'Class has been deleted.'
+        });
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
-    }
-});
-
-router.put("/:id", auth, async (req, res) => {
-    try {
-        const updateClass = await Class.findOne({
-            classId: req.params.id
-        });
-    } catch (err) {
-
     }
 })
 
